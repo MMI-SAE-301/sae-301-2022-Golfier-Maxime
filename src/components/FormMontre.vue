020e38491f61811e2a664b4bfbc49f318f0123e3
Maxime Golfier <maxime.golfier@edu.univ-fcomte.fr>
2022-10-25 13:49:53 +0200

    OAuth Avec Google fonctionnable

--- src/components/FormMontre.vue
+++ src/components/FormMontre.vue
@@ -9,53 +9,44 @@ import { useRouter } from "vue-router";
 const router = useRouter();
 
 const props = defineProps<{
-    data?: Basket;
+    data?: Montre;
     id?: string;
 }>();
 
-const chaussure = ref<Basket>(props.data ?? {});
+const montre = ref<Montre>(props.data ?? {});
 
-async function upsertBasket(dataForm, node) {
-    const { data, error } = await supabase.from("chaussure").upsert(dataForm);
+async function upsertMontre(dataForm, node) {
+    const { data, error } = await supabase.from("montre").upsert(dataForm);
     if (error) node.setErrors([error.message]);
     else {
         node.setErrors([]);
-        router.push({ name: "basket" });
+        router.push({ name: "montre" });
     }
 }
 
 if (props.id) {
-    // On charge les données de la maison
+    // On charge les données de la montre
     let { data, error } = await supabase
-        .from("chaussure")
+        .from("montre")
         .select("*")
-        .eq("id_chaussure", props.id);
+        .eq("montre", props.id);
     if (error || !data)
-        console.log("n'a pas pu charger le table chaussure :", error);
-    else chaussure.value = data[0];
+        console.log("n'a pas pu charger le table montre :", error);
+    else montre.value = data[0];
 }
 </script>
 
 <template>
     <div class="p-2">
-        <ul class="flex gap-1">
-            <li><a href="#profil">Profil</a></li>
-            <li><a href="#dessus">Dessus</a></li>
-        </ul>
-        <div class="carousel w-64">
-            <BasketProfil class="carousel-item w-64" v-bind="chaussure" id="profil" />
-            <BasketDessus class="carousel-item w-64" v-bind="chaussure" id="dessus" />
-        </div>
-        <FormKit type="form" v-model="chaussure" @submit="upsertBasket">
 
-            <FormKitListColors name="semelle" label="Semelle" />
-            <FormKitListColors name="empeigne" label="Empeigne" />
-            <FormKitListColors name="pointe" label="Pointe" />
-            <FormKitListColors name="oeillet" label="Oeillet" />
-            <FormKitListColors name="bande" label="Bande" />
-            <FormKitListColors name="languette" label="Languette" />
-            <FormKitListColors name="lacet" label="Lacet" />
-            <FormKitListColors name="trimestre" label="Trimestre" />
+        <div class="w-64">
+            <Montre class="w-64" v-bind="montre" id="profil" />
+        </div>
+        <FormKit type="form" v-model="montre" @submit="upsertMontre">
+
+            <FormKitListColors name="bracelet" label="Bracelet" />
+            <FormKitListColors name="boitier" label="Boitier" />
+            <FormKitListColors name="ecran" label="Ecran" />
             <!-- matériaux -->
             <!-- <FormKit name="materiaux" label="Matériaux" value='#FFFFFF' type="radio" :options="materiaux"
                 :sections-schema="{
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,9 @@
+<script setup lang="ts">
+import LoginLogout from '@/components/LoginLogout.vue';
+</script>
+
 <template>
+  <LoginLogout />
   <section>
     <h1 class="text-2xl">
       Bonjour<code class="font-mono text-base">/src/pages/index.vue</code>
