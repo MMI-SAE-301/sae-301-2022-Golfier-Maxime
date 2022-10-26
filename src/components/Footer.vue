8115d187794014db3a986dcb207c67c1b8c2db52
Maxime Golfier <maxime.golfier@edu.univ-fcomte.fr>
2022-10-26 08:56:02 +0200

    Footer fini

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,20 +1,21 @@
 <script setup></script>
 <template>
-    <div class="">
-        <p>TiK•TaK</p>
-        <div>
+    <div class="pb-8 pt-8">
+        <p class="text-black josefin text-[45px] ml-[45px] text-center">TiK•TaK</p>
+        <div class="flex justify-center gap-16 mt-6">
             <img src="public\discord.svg">
             <img src="public\insta.svg">
             <img src="public\tiktok.svg">
             <img src="public\twitch.svg">
             <img src="public\twitter.svg">
         </div>
-        <div>
-
+        <div class="flex justify-center">
+            <div class="ligne_footer mt-6 mb-6 ">
+            </div>
         </div>
-        <div>
-            <p></p>
-            <p></p>
+        <div class="flex justify-center gap-16 ">
+            <p class="font-montserrat font-semibold">Politique de confidentialité</p>
+            <p class="font-montserrat font-semibold">@2022 TikTak. All rights reserved</p>
         </div>
     </div>
 </template>
@@ -23,5 +24,11 @@
     font-family: josefin-sans, sans-serif;
     font-weight: 700;
     font-style: normal;
+}
+
+.ligne_footer {
+    height: 3px;
+    width: 800px;
+    background-color: black;
 }
 </style>
