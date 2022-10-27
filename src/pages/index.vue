ab3ff929dedccd5488ea59061f083a014e9e233f
Maxime Golfier <maxime.golfier@edu.univ-fcomte.fr>
2022-10-27 13:29:31 +0200

    index.vue responsive

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,13 +3,13 @@ import LoginLogout from '@/components/LoginLogout.vue';
 </script>
 
 <template>
-  <div class="lg:mt-40 lg:mb-40">
+  <div class="lg:mt-40 mt-9 lg:mb-40">
     <!-- Section 1 -->
-    <div class="lg:flex lg:ml-[169px] lg:mr-[169px]">
-      <div class="flex flex-col justify-between">
-        <div>
-          <h1 class="font-montserrat font-bold text-[60px]">LE TEMPS EST MODIFIÉ</h1>
-          <h1 class="font-montserrat font-bold text-[60px]">AVEC TOI !</h1>
+    <div class="lg:flex lg:ml-[169px] ml-3  lg:mr-[169px]">
+      <div class="flex flex-col justify-between gap-2">
+        <div class="">
+          <h1 class="font-montserrat font-bold lg:text-[60px] text-2xl">LE TEMPS EST MODIFIÉ</h1>
+          <h1 class="font-montserrat font-bold lg:text-[60px] text-2xl">AVEC TOI !</h1>
         </div>
         <p class="font-montserrat font-light text-2xl">TiK•TaK, est le site pour créer ta propre montre connecter !</p>
         <router-link to="/custom">
@@ -21,15 +21,15 @@ import LoginLogout from '@/components/LoginLogout.vue';
       </div>
     </div>
     <!-- Section 2 -->
-    <div class="mt-40">
-      <div class="lg:flex lg:ml-[169px] lg:mr-[169px] ">
+    <div class="lg:mt-40 mt-10">
+      <div class="lg:flex lg:ml-[169px] lg:mr-[169px] ml-3 ">
         <div class="lg:w-[400px]">
           <img src="../assets/img/carre-tiktak.png">
         </div>
         <div class="flex flex-col justify-between">
           <div>
-            <p class="font-montserrat font-medium text-[60px]">Votre TIK TAK </p>
-            <p class="font-montserrat font-medium text-[60px]"> est forcement perle rare</p>
+            <p class="font-montserrat font-medium lg:text-[60px] text-2xl">Votre TIK TAK </p>
+            <p class="font-montserrat font-medium lg:text-[60px] text-2xl"> est forcement perle rare</p>
           </div>
           <p class="font-montserrat font-light text-2xl">Vue que c’est vous qui faite votre montre, elle sera unique est
             spécialement conçu pour vous.
